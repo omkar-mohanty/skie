3f6d9723897b64d650e71278064983ad61a18017
omkar-mohanty <franzohouser@gmail.com>
2026-02-07 20:13:12 +0530

    partial progress on sync engine

--- skie-index/src/lib.rs
+++ skie-index/src/lib.rs
@@ -1,179 +1,69 @@
-pub mod index_store;
-
-use blake3::Hash;
-use fastcdc::v2020::{ChunkData, StreamCDC};
-use rayon::{
-    ThreadPool, ThreadPoolBuildError, ThreadPoolBuilder,
-    iter::{ParallelBridge, ParallelIterator},
+mod hash_engine;
+use fastcdc::ronomon::Chunk;
+pub use hash_engine::{HashEngine, HashEngineError};
+use loro::{LoroDoc, LoroList};
+use rayon::iter::{ParallelBridge, ParallelIterator};
+use skie_common::{ChunkIndex, ChunkMetadata, FileID, FileMetadata};
+use std::{
+    collections::{BTreeMap, HashMap},
+    path::PathBuf,
 };
-use skie_common::IndexEngineConfig;
-use std::{fs::File, path::PathBuf};
-use thiserror::Error;
+use uuid::Uuid;
 
 pub(crate) type Result<E> = std::result::Result<E, HashEngineError>;
+pub(crate) type DeviceID = Uuid;
 
-#[derive(Debug)]
-pub struct HashData {
-    pub index: usize,
-    pub chunk: Vec<u8>,
-    pub hash: Hash,
+pub struct SyncFile {
+    pub file_metadata: FileMetadata,
+    pub doc: LoroDoc,
+    pub changes: LoroList,
 }
 
-impl PartialEq for HashData {
-    fn eq(&self, other: &Self) -> bool {
-        self.index == other.index && self.hash == other.hash
-    }
-}
-
-pub struct HashEngine {
-    config: IndexEngineConfig,
-    thread_pool: ThreadPool,
-}
-
-impl HashEngine {
-    pub fn new(config: IndexEngineConfig) -> Result<Self> {
-        let thread_pool = ThreadPoolBuilder::new()
-            .num_threads(config.num_threads)
-            .thread_name(|i| format!("skie-index-{}", i))
-            .build()?;
-        Ok(Self {
-            config,
-            thread_pool,
-        })
-    }
-
-    pub fn process(&self, source: PathBuf) -> impl Iterator<Item = Result<HashData>> {
-        let (tx, rx) =
-            crossbeam_channel::bounded::<Result<(usize, ChunkData)>>(self.config.channel_size);
-        let (output_tx, output_rx) = crossbeam_channel::bounded(self.config.channel_size);
-
-        let min_chunk_size = self.config.min_chunk_size;
-        let avg_chunk_size = self.config.avg_chunk_size;
-        let max_chunk_size = self.config.max_chunk_size;
-
-        self.thread_pool.spawn(move || {
-            let source = match File::open(source) {
-                Ok(file) => file,
-                Err(msg) => {
-                    let err = Err(HashEngineError::IoError(msg));
-                    let _ = tx.send(err);
-                    return;
-                }
-            };
-
-            let stream_cdc = StreamCDC::new(source, min_chunk_size, avg_chunk_size, max_chunk_size);
-
-            for (index, chunk_res) in stream_cdc.enumerate() {
-                let res = match chunk_res {
-                    Ok(chunk_data) => Ok((index, chunk_data)),
-                    Err(msg) => Err(HashEngineError::ChunkError(msg)),
-                };
-
-                let _ = tx.send(res);
-            }
-        });
-
-        self.thread_pool.spawn(move || {
-            rx.iter().par_bridge().for_each(|chunk_res| {
-                let res = match chunk_res {
-                    Ok((index, chunkdata)) => {
-                        let chunk = chunkdata.data;
-                        let hash = blake3::hash(&chunk);
-                        let hash_data = HashData { index, hash, chunk };
-                        Ok(hash_data)
-                    }
-                    Err(msg) => Err(msg),
-                };
-
-                let _ = output_tx.send(res);
-            });
-        });
-
-        output_rx.into_iter()
-    }
-}
-
-#[derive(Debug, Error)]
-pub enum HashEngineError {
-    #[error("Index Engine: Io Error")]
-    IoError(#[from] std::io::Error),
-    #[error("Index Engine: Send Data Error")]
-    SendDataError(#[from] crossbeam_channel::SendError<ChunkData>),
-    #[error("Index Engine: Chunk error")]
-    ChunkError(#[from] fastcdc::v2020::Error),
-    #[error("Index Engine: ThreadPool error")]
-    ThreadPoolError(#[from] ThreadPoolBuildError),
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rand::{RngCore, rng};
-    use std::io::Write;
-    use tempfile::NamedTempFile;
-
-    fn test_config() -> IndexEngineConfig {
-        IndexEngineConfig {
-            num_threads: 4,
-            min_chunk_size: 1024 * 16,
-            avg_chunk_size: 1024 * 32,
-            max_chunk_size: 1024 * 64,
-            channel_size: 64,
-            ..Default::default()
+impl SyncFile {
+    pub fn new(id: DeviceID, file_metadata: FileMetadata) -> Self {
+        let doc = LoroDoc::new();
+        let changes = doc.get_list(id.to_string());
+        Self {
+            file_metadata,
+            doc,
+            changes,
         }
     }
 
-    #[test]
-    fn test_empty_file() -> Result<()> {
-        let engine = HashEngine::new(test_config())?;
-        let file = NamedTempFile::new()?;
-        let source = file.path().to_path_buf();
-        let results: Vec<_> = engine.process(source).collect();
-        assert!(results.is_empty(), "Empty file should produce zero chunks");
-        Ok(())
+    pub fn sync_new_chunks(
+        mut self,
+        new_tree: BTreeMap<ChunkIndex, ChunkMetadata>,
+    ) -> BTreeMap<ChunkIndex, ChunkMetadata> {
+        let mut old_tree = self.file_metadata.manifest_tree;
+        let removed_enries = old_tree
+            .extract_if(.., |key, _| new_tree.contains_key(key))
+            .collect::<BTreeMap<ChunkIndex, ChunkMetadata>>();
+        self.file_metadata.manifest_tree = new_tree;
+        removed_enries
     }
+}
 
-    #[test]
-    fn test_small_file_single_chunk() -> Result<()> {
-        let engine = HashEngine::new(test_config())?;
-        let data = vec![0u8; 1024];
-        let mut file = NamedTempFile::new()?;
-        let _ = file.write_all(&data);
-        let source = file.path().to_path_buf();
-        // Iterators let us just take the first item
-        let mut it = engine.process(source);
-        let first = it.next().expect("Should have one chunk")?;
+pub struct SyncEngine {
+    hash_engine: HashEngine,
+    file_map: HashMap<FileID, FileMetadata>,
+}
 
-        assert_eq!(first.hash, blake3::hash(&data));
-        assert_eq!(first.index, 0);
-        Ok(())
+impl SyncEngine {
+    pub fn new() {
+        let loro_doc = LoroDoc::new();
     }
+}
 
-    #[test]
-    fn test_determinism() -> Result<()> {
-        let engine = HashEngine::new(test_config())?;
-        let mut data = vec![0u8; 1024 * 100];
-        rng().fill_bytes(&mut data);
-        let mut file = NamedTempFile::new()?;
-        let _ = file.write_all(&data)?;
-
-        let source_first = file.path().to_path_buf();
-        let source_second = file.path().to_path_buf();
-
-        // Determinism is tricky with par_bridge because order is random.
-        // We collect and sort by index to verify the content is identical.
-
-        let mut res_1: Vec<_> = engine.process(source_first).map(|r| r.unwrap()).collect();
-        let mut res_2: Vec<_> = engine.process(source_second).map(|r| r.unwrap()).collect();
-
-        res_1.sort_by_key(|d| d.index);
-        res_2.sort_by_key(|d| d.index);
-
-        assert_eq!(res_1.len(), res_2.len());
-        for (a, b) in res_1.iter().zip(res_2.iter()) {
-            assert_eq!(a.hash, b.hash);
-            assert_eq!(a.index, b.index);
-        }
-        Ok(())
-    }
+pub fn calculate_diff(engine: &HashEngine, source: PathBuf, metadata: &FileMetadata) {
+    let manifest_map = &metadata.manifest_tree;
+    let iter = engine
+        .process(source)
+        .par_bridge()
+        .filter(|hash_res| match hash_res {
+            Ok(hash_data) => match manifest_map.get(&hash_data.index) {
+                Some(chunk_metadata) => *chunk_metadata == *hash_data,
+                None => true,
+            },
+            Err(_) => true,
+        });
 }
