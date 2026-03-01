2aedc27113d99846f5d1b95c207b5f125805298c
omkar-mohanty <franzohouser@gmail.com>
2026-03-01 23:43:29 +0530

    update logic for handling atomic file updates

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,12 +1,18 @@
 use anyhow::Result;
+use camino::Utf8PathBuf;
 use common::*;
 use crossbeam_channel::unbounded;
 use notify_debouncer_full::{
-    new_debouncer,
-    notify::{EventKind, RecursiveMode},
+    DebouncedEvent, new_debouncer,
+    notify::{Event, EventKind, RecursiveMode},
 };
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf, time::Duration};
+use std::{
+    fs,
+    iter::Peekable,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 use store::ChunkConfig;
 
 #[derive(Deserialize, Serialize)]
@@ -14,6 +20,81 @@ struct ServiceConfig {
     chunk_config: ChunkConfig,
     sync_dir: Vec<PathBuf>,
     debounce_ms: u64,
+}
+
+pub struct OsEvent {
+    kind: OsEventKind,
+    paths: Vec<Utf8PathBuf>,
+    time: Instant,
+}
+
+pub enum OsEventKind {
+    Other,
+    Access,
+    Any,
+    Create,
+    Update,
+    Remove,
+}
+
+pub fn build_events_iter<I: Iterator<Item = DebouncedEvent>>(
+    mut events: Peekable<I>,
+) -> Vec<OsEvent> {
+    let mut os_events = vec![];
+    while let Some(debounced_event) = events.next() {
+        use EventKind::*;
+        let DebouncedEvent { event, time } = debounced_event;
+        let Event { kind, paths, .. } = event;
+
+        let paths: Vec<Utf8PathBuf> = paths
+            .into_iter()
+            .map(|path| Utf8PathBuf::from_path_buf(path).unwrap())
+            .collect();
+
+        if let Remove(_) = kind {
+            // Check if the NEXT event is a Create for the SAME path
+            let is_atomic_update = events.peek().map_or(false, |next_event| {
+                matches!(next_event.event.kind, Create(_)) && next_event.event.paths == paths // Paths must match!
+            });
+
+            if is_atomic_update {
+                // Consume the paired 'Create' event from the iterator
+                events.next();
+
+                os_events.push(OsEvent {
+                    kind: OsEventKind::Update,
+                    paths,
+                    time,
+                });
+            } else {
+                // It was just a normal delete
+                os_events.push(OsEvent {
+                    kind: OsEventKind::Remove,
+                    paths,
+                    time,
+                });
+            }
+        } else {
+            // Not a remove, map directly
+            let kind = kind.into();
+            os_events.push(OsEvent { kind, paths, time });
+        }
+    }
+    os_events
+}
+
+impl From<EventKind> for OsEventKind {
+    fn from(value: EventKind) -> Self {
+        use EventKind::*;
+        match value {
+            Create(_) => OsEventKind::Create,
+            Remove(_) => OsEventKind::Remove,
+            Modify(_) => OsEventKind::Update,
+            Access(_) => OsEventKind::Access,
+            Any => OsEventKind::Any,
+            Other => OsEventKind::Other,
+        }
+    }
 }
 
 impl Default for ServiceConfig {
@@ -72,22 +153,26 @@ async fn main() -> Result<()> {
 
     while let Ok(events) = event_receiver.recv()? {
         //TODO Need to handle a special case where the sync directory is deleted while skie is running.
-        let events_iter = events.iter().filter(|debounced_event| {
-            // 1. Only care about data-changing events
-            let is_valid_kind = matches!(
-                debounced_event.event.kind,
-                EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_)
-            );
+        let events_iter = events
+            .into_iter()
+            .filter(|debounced_event| {
+                // 1. Only care about data-changing events
+                let is_valid_kind = matches!(
+                    debounced_event.event.kind,
+                    EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_)
+                );
 
-            // 2. Ignore anything inside the .config folder
-            let is_not_internal = !debounced_event
-                .event
-                .paths
-                .iter()
-                .any(|path| path.components().any(|c| c.as_os_str() == ".config"));
+                // 2. Ignore anything inside the .config folder
+                let is_not_internal = !debounced_event
+                    .event
+                    .paths
+                    .iter()
+                    .any(|path| path.components().any(|c| c.as_os_str() == ".config"));
 
-            is_valid_kind && is_not_internal
-        });
+                is_valid_kind && is_not_internal
+            })
+            .peekable();
+        let os_events = build_events_iter(events_iter);
     }
 
     Ok(())
