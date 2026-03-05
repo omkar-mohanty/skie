a5605e6328b541258e1981363b07bb2453a7276f
omkar <franzohouser@gmail.com>
2026-03-05 12:38:17 +0530

    add events to handle events

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,101 +1,24 @@
 use anyhow::Result;
-use camino::Utf8PathBuf;
 use common::*;
 use crossbeam_channel::unbounded;
 use notify_debouncer_full::{
-    DebouncedEvent, new_debouncer,
-    notify::{Event, EventKind, RecursiveMode},
+    new_debouncer,
+    notify::{EventKind, RecursiveMode},
 };
 use serde::{Deserialize, Serialize};
 use std::{
     fs,
-    iter::Peekable,
     path::PathBuf,
-    time::{Duration, Instant},
+    time::Duration,
 };
 use store::ChunkConfig;
+use diff_d::build_events_iter;
 
 #[derive(Deserialize, Serialize)]
 struct ServiceConfig {
     chunk_config: ChunkConfig,
     sync_dir: Vec<PathBuf>,
     debounce_ms: u64,
-}
-
-pub struct OsEvent {
-    pub kind: OsEventKind,
-    pub paths: Vec<Utf8PathBuf>,
-    pub time: Instant,
-}
-
-pub enum OsEventKind {
-    Other,
-    Access,
-    Any,
-    Create,
-    Update,
-    Remove,
-}
-
-pub fn build_events_iter<I: Iterator<Item = DebouncedEvent>>(
-    mut events: Peekable<I>,
-) -> Vec<OsEvent> {
-    let mut os_events = vec![];
-    while let Some(debounced_event) = events.next() {
-        use EventKind::*;
-        let DebouncedEvent { event, time } = debounced_event;
-        let Event { kind, paths, .. } = event;
-
-        let paths: Vec<Utf8PathBuf> = paths
-            .into_iter()
-            .map(|path| Utf8PathBuf::from_path_buf(path).unwrap())
-            .collect();
-
-        if let Remove(_) = kind {
-            // Check if the NEXT event is a Create for the SAME path
-            let is_atomic_update = events.peek().map_or(false, |next_event| {
-                matches!(next_event.event.kind, Create(_)) && next_event.event.paths == paths // Paths must match!
-            });
-
-            if is_atomic_update {
-                // Consume the paired 'Create' event from the iterator
-                events.next();
-
-                os_events.push(OsEvent {
-                    kind: OsEventKind::Update,
-                    paths,
-                    time,
-                });
-            } else {
-                // It was just a normal delete
-                os_events.push(OsEvent {
-                    kind: OsEventKind::Remove,
-                    paths,
-                    time,
-                });
-            }
-        } else {
-            // Not a remove, map directly
-            let kind = kind.into();
-            os_events.push(OsEvent { kind, paths, time });
-        }
-    }
-    os_events
-}
-
-/// TODO: Add additonal information to `OsEventKind`
-impl From<EventKind> for OsEventKind {
-    fn from(value: EventKind) -> Self {
-        use EventKind::*;
-        match value {
-            Create(_) => OsEventKind::Create,
-            Remove(_) => OsEventKind::Remove,
-            Modify(_) => OsEventKind::Update,
-            Access(_) => OsEventKind::Access,
-            Any => OsEventKind::Any,
-            Other => OsEventKind::Other,
-        }
-    }
 }
 
 impl Default for ServiceConfig {
